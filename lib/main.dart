0b46e58aeb34064ec3144cd23c02a7b957e1431d
Leen Majali <leenmajali960@gmail.com>
2026-03-25 20:40:55 +0300

    TextStyle.training

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Title")),
+        body: Text(
+          "leen majali",
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 40.6,
+            fontWeight: FontWeight.w100,
+            backgroundColor: Colors.black87,
+          ),
+        ),
+      ),
+    );
+  }
+}
