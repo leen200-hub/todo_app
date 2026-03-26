33956db052bf47c1687c8b83a372e8f27b1894f3
Leen Majali <leenmajali960@gmail.com>
2026-03-26 18:53:24 +0300

    container.t

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Title")),
-        body: Text(
-          "my first update on github",
-          style: TextStyle(
+        body: Container(
+          decoration: BoxDecoration(
             color: Colors.red,
-            fontSize: 40.6,
-            fontWeight: FontWeight.w100,
-            backgroundColor: Colors.black87,
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            border: Border.all(color: Colors.amberAccent, width: 10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blueAccent,
+                offset: Offset(10, 10),
+                spreadRadius: 2,
+                blurRadius: 10,
+              ),
+              BoxShadow(
+                color: Color.fromARGB(255, 77, 211, 36),
+                offset: Offset(-10, 10),
+                spreadRadius: 2,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          width: 300,
+          height: 300,
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(20),
+          child: const Text(
+            "my seconde update on github",
+            style: TextStyle(
+              fontSize: 40.6,
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.w100,
+              backgroundColor: Colors.black87,
+            ),
           ),
         ),
       ),
